9a9a65175c892254296b85e03d708891c949b2c8
LuisLodosoGimenez <luislodosogimenez@gmail.com>
2024-04-14 01:13:52 +0200

    personal-info

--- userProfileSpringCarrot/src/app/app.routes.ts
+++ userProfileSpringCarrot/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HomePageComponent } from './home-page/home-page.component'
 import { UserProfileInitComponent } from './user-profile/user-profile-init/user-profile-init.component'
 import { UserProfileOrderListComponent } from './user-profile/user-profile-order-list/user-profile-order-list.component'
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component'
+import { UserProfilePersonalInfoComponent } from './user-profile/user-profile-personal-info/user-profile-personal-info.component'
 
 export const routes: Routes = [
   {
@@ -17,6 +18,14 @@ export const routes: Routes = [
     children: [
       {
         path: '',
+        component: UserProfileInitComponent,
+      },
+      {
+        path: 'personal-info',
+        component: UserProfilePersonalInfoComponent,
+      },
+      {
+        path: 'order-list',
         component: UserProfileOrderListComponent,
       },
     ],
---
+++ userProfileSpringCarrot/src/app/user-profile/user-profile-personal-info/user-profile-personal-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-personal-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-profile-personal-info.component.html',
+  styleUrl: './user-profile-personal-info.component.css'
+})
+export class UserProfilePersonalInfoComponent {
+
+}
