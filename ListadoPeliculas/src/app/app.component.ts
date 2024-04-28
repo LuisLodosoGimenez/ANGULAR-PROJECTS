7d7da93b5ab8049a11849e73b5b40c07b95e1d0f
LuisLodosoGimenez <luislodosogimenez@gmail.com>
2024-04-28 19:24:28 +0200

    new back-image and a little info

--- ListadoPeliculas/src/app/app.component.ts
+++ ListadoPeliculas/src/app/app.component.ts
@@ -9,6 +9,12 @@ import {
 import { ReactiveFormsModule, FormControl } from '@angular/forms'
 import { CommonModule } from '@angular/common'
 import { Router } from '@angular/router'
+import { TestBed } from '@angular/core/testing'
+
+enum State {
+  active = 1,
+  inactive = 0,
+}
 
 @Component({
   selector: 'app-root',
@@ -22,14 +28,40 @@ export class AppComponent {
   static movieSearch: MovieSearchByName = new MovieSearch()
   textSearch = new FormControl()
   bgImageUrl = ''
+  searchState: State = State.inactive
+  infoState: State = State.inactive
+  leftMarginOfList = 40
+  movie?: Movie = undefined
+  width = '2/4'
 
   constructor(
     private movieApiService: MovieApiService,
     private router: Router,
   ) {}
 
+  mouseOverMovieTitle(url: string) {
+    this.changeBgImageUrl(url)
+  }
+
+  clickMovieTitle(movie: Movie) {
+    this.movie = movie
+    this.infoState = 1
+    this.changeLeftMarginOfList()
+    this.changeWidthOfList()
+  }
+
   changeBgImageUrl(url: string) {
     this.bgImageUrl = url
+  }
+
+  changeLeftMarginOfList() {
+    if (this.infoState) this.leftMarginOfList = 4
+    else this.leftMarginOfList = 40
+  }
+
+  changeWidthOfList() {
+    if (this.infoState) this.width = '2/5'
+    else this.width = '2/4'
   }
 
   returnMovies(): Movie[] {
@@ -37,6 +69,11 @@ export class AppComponent {
   }
 
   getMovies() {
+    if (this.textSearch.value == '') {
+      this.searchState = State.inactive
+      this.infoState = State.inactive
+    } else this.searchState = State.active
+
     this.router.navigate(['/about'])
     this.movieApiService.getMovies(this.textSearch.value).subscribe({
       next: (response) => {
